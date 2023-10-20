64d5b1f4e6a897e50047d76ea2eb4c08b2ac19a7
Madu <137858554+MaduOfficial@users.noreply.github.com>
2023-10-19 21:50:05 -0300

    Add files via upload

---
+++ MatrizIrregular.java
@@ -0,0 +1,38 @@
+package com.madu.Matrizes.arrays;
+
+import java.util.Scanner;
+
+public class MatrizIrregular {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o número de pessoas que serão entrevistadas:");
+		int numEntrevistados = scan.nextInt();
+		
+		String[][] nomeFilhos = new String[numEntrevistados][];
+		
+		for (int i=0; i<nomeFilhos.length; i++) {
+			
+			System.out.println("Entre com a contidade de filhos");
+			int qtdFilhos = scan.nextInt();
+			
+			nomeFilhos[i] = new String[qtdFilhos];
+			
+			for (int j=0; j<nomeFilhos[i].length; j++) {
+				
+				System.out.println("Digite o nome do filho " + (j+1));
+				
+				nomeFilhos[i][j] = scan.next();
+			}
+		}
+		
+		for (int i=0; i<nomeFilhos.length; i++) {
+			System.out.println("Pessoa " + i + " tem " + nomeFilhos[i].length + " filhos");
+			for (int j=0; j<nomeFilhos[i].length; j++) {
+				System.out.println(nomeFilhos[i][j]);
+			}
+		}
+	}
+}
